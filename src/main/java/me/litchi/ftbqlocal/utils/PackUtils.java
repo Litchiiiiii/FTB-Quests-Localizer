3ecea48e87be9a6cbe9213aeb844000572eccaaa
litchiiiiiii <1592282237@qq.com>
2026-01-26 15:46:50 +0800

    fix bug and release 3.2.3

--- src/main/java/me/litchi/ftbqlocal/utils/PackUtils.java
+++ src/main/java/me/litchi/ftbqlocal/utils/PackUtils.java
@@ -16,7 +16,7 @@ public class PackUtils {
 
         String zipFileName = outputName;
         JsonObject packObject = generatePackMcmeta(Constants.PackMCMeta.DESCRIPTION, Constants.PackMCMeta.PACKFORMAT);
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
         String jsonOutput = gson.toJson(packObject);
         File packMcMeta = new File(Constants.PackMCMeta.FILEPATH);
         FileUtils.write(packMcMeta, jsonOutput.toString(), StandardCharsets.UTF_8);
