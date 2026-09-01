eb514e98068a386387528ac04d26ee1935617a25
Litchi <1592282237@qq.com>
2026-09-01 20:32:09 +0800

    fixed bug and release 3.2.4

--- src/main/java/me/litchi/ftbqlocal/commands/FTBQLangConvert.java
+++ src/main/java/me/litchi/ftbqlocal/commands/FTBQLangConvert.java
@@ -86,6 +86,6 @@ public class FTBQLangConvert {
     {
         File fe = new File(parent, lang.toLowerCase(Locale.ROOT) + ".json");
         FileUtils.write(fe, FtbQuestLocalizerMod.gson.toJson(HandlerCounter.transKeys), StandardCharsets.UTF_8);
-        PackUtils.createResourcePack(parent, FMLPaths.GAMEDIR.get().toFile()+"\\FTBLang\\FTB-Quests-Localization-Resourcepack.zip");
+        PackUtils.createResourcePack(parent, FMLPaths.GAMEDIR.get().toFile()+File.separator+"FTBLang"+File.separator+"FTB-Quests-Localization-Resourcepack.zip");
     }
 }
