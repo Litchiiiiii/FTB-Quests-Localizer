46e43c90cecdd8f286b387c63b5d8ec5078df017
Litchi <1592282237@qq.com>
2026-09-01 23:50:22 +0800

    fixed bug and release 3.2.4

--- src/main/java/me/litchi/ftbqlocal/utils/PackUtils.java
+++ src/main/java/me/litchi/ftbqlocal/utils/PackUtils.java
@@ -18,7 +18,7 @@ public class PackUtils {
 
         try (ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(outputName))) {
             for (File listFile : Objects.requireNonNull(file.listFiles())) {
-                addToZip("assets\\minecraft\\lang\\", listFile, zipOut);
+                addToZip("assets"+File.separator+"minecraft"+File.separator+"lang"+File.separator, listFile, zipOut);
             }
             addToZip("", packMcMeta, zipOut);
 
